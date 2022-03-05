83ddb9c8d2d947e0439cdb32ae21ab1aab1141eb
rkutaraev@list.ru <rkutaraev@list.ru>
2022-03-05 09:51:17 +0300

    Запросить у пользователя минимальную и максимальную температуру за сутки и вывести среднесуточную температуру.

--- Lesson_work2/Program.cs
+++ Lesson_work2/Program.cs
@@ -10,6 +10,21 @@ namespace Lesson_work2
     {
         static void Main(string[] args)
         {
+            string maximalTemperature;
+            Console.WriteLine("Введите, максимальную температуру ");
+            maximalTemperature = Console.ReadLine();
+            int x = Convert.ToInt32(maximalTemperature);
+            Console.WriteLine("максимальная температура составляет " + maximalTemperature );
+
+            string minimalTemperature;
+            Console.WriteLine("Введите, минимальную температуру ");
+            minimalTemperature = Console.ReadLine();
+            int y = Convert.ToInt32(minimalTemperature);
+            Console.WriteLine("Максимальнуа температура составляет " + minimalTemperature );
+
+            int result = (x + y) / 2;
+            Console.WriteLine("Средняя температура за сутки " + result);
+            Console.ReadLine();
         }
     }
 }
